37506871debdcc61a770761007796de351539561
Lenix <lenix01x@gmail.com>
2024-10-13 22:38:33 +0100

    A lot of thingsssss

---
+++ scripts/product.js
@@ -0,0 +1,326 @@
+const products = [{
+  id: 'e0f223c5-0b28-464b-8f06-716380949e58',
+  image:'../images/Veh/hyper/veh1.png',
+  name: 'Bugatti Bolide',
+  price: 2999,
+  rent: '--.--'
+}, {
+  id: 'c94c9ee2-4101-4573-8d95-e4d09d088808',
+  image:'../images/Veh/hyper/veh10.png',
+  name: 'Koenigsegg Jesko',
+  price: 2499,
+  rent: '--.--'
+}, {
+  id: '39d83293-22a0-468b-a044-82822e196c85',
+  image:'../images/Veh/hyper/veh6.png',
+  name: 'Pagani Imola',
+  price: 2499 ,
+  rent: '--.--'
+}, {
+  id: 'd6ebfb2e-60fa-42ba-8345-8e2e28d4a2e5',
+  image:'../images/Veh/hyper/veh3.png',
+  name: 'Ferrari F150 LaFerrari',
+  price: 1999,
+  rent: '--.--'
+}, {
+  id: '9253e138-3161-491d-851d-b20699315503',
+  image:'../images/Veh/hyper/veh8.png',
+  name: 'Mclaren Senna',
+  price: 1999,
+  rent: '--.--'
+}, {
+  id: 'db72fefe-7b38-4c5b-aa9c-bf57b5fc7130',
+  image:'../images/Veh/super/veh19.png',
+  name: 'Lamborghini Aventador LP700',
+  price: 1499,
+  rent: '--.--'
+}, {
+  id: 'f176c05c-b59a-498a-b0df-7bdd87651bd0',
+  image:'../images/Veh/super/veh11.png',
+  name: 'Audi R8 Hycade',
+  price: 1499,
+  rent: '--.--'
+}, {
+  id: '6e0f4a51-c0b0-4613-a8c6-f56478946da3',
+  image:'../images/Veh/super/veh25.png',
+  name: 'Porsche 911 GT3',
+  price: 1499,
+  rent: '--.--'
+}, {
+  id: '6c237dd2-1439-4b4d-800e-fd53d84730d7',
+  image:'../images/Veh/super/veh13.png',
+  name: 'Aston Martin Vulcan',
+  price: 1499,
+  rent: '--.--'
+}, {
+  id: '9b83fc5f-09a5-4dc9-a958-1a6bb1fdcfb3',
+  image:'../images/Veh/super/veh27.png',
+  name: 'Mclaren 720s',
+  price: 1499,
+  rent: '--.--'
+}, {
+  id: '3382a500-7e81-4ee8-802e-4ca236afe3d0',
+  image:'../images/Veh/super/veh16.png',
+  name: 'Bentley Continental Supersports',
+  price: 1499,
+  rent: '--.--'
+}, {
+  id: 'fbd86ada-408d-46cb-ad09-27c9df2609ce',
+  image:'../images/Veh/super/veh23.png',
+  name: 'Mercedes Benz-AMG GT Black Series',
+  price: 1499,
+  rent: '--.--'
+}, {
+  id: 'a485771c-3547-45f9-bdd9-cef6e075b637',
+  image:'../images/Veh/super/veh17.png',
+  name: 'Lamborghini Huracan',
+  price: 1499,
+  rent: '--.--'
+}, {
+  id: 'c7ea1df9-c439-4f48-8001-5fb7f3092901',
+  image:'../images/Veh/super/veh22.png',
+  name: 'Mclaren 675LT',
+  price: 1499,
+  rent: '--.--'
+}, {
+  id: 'ea585351-1761-4cef-ac7b-b44f6a9020c0',
+  image:'../images/Veh/sport/veh41.png',
+  name: 'Mercedes Benz-AMG C63',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: '82c17685-572a-4eb9-9ec0-bb58d642a8cb',
+  image:'../images/Veh/sport/veh51.png',
+  name: 'BMW M5 C5',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: 'f95e5ed7-5823-4301-b581-3e6795b63a74',
+  image:'../images/Veh/sport/veh53.png',
+  name: 'Porsche Panamera Turbo',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: '28da33b5-8628-46cc-a1fe-1f8be2e8e798',
+  image:'../images/Veh/sport/veh31.png',
+  name: 'Ford GT',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: 'de27e082-0799-45bb-9de9-cbfb48af4976',
+  image:'../images/Veh/sport/veh43.png',
+  name: 'Mercedes Benz-AMG GT63 S',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: '3d510307-e4cc-40cc-b74a-3c968327116f',
+  image:'../images/Veh/sport/veh33.png"',
+  name: 'Ford Mustang',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: '95453e16-6691-405a-bb73-ca52b7e69f87',
+  image:'../images/Veh/sport/veh45.png',
+  name: 'Nissan GTR',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: '7b73a46c-8d7a-4d98-8447-c6bd80c14a78',
+  image:'../images/Veh/sport/veh35.png',
+  name: 'Lexus LFA',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: '823c67cb-07f4-47e8-af6d-096e984de1d3',
+  image:'../images/Veh/sport/veh37.png',
+  name: 'Maseratti Gran Turismo LW',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: '07ce2c60-2584-4b09-b877-da6a6d2f855b',
+  image:'../images/Veh/sport/veh29.png',
+  name: 'Chevrolet Camaro ZL1 Hennessey',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: '14c13f08-2163-4b1a-b003-e1f970cfea93',
+  image:'../images/Veh/sport/veh47.png',
+  name: 'Toyota Supra A90',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: 'fdb34ba7-1d89-4724-8e48-9b7394ec78c0',
+  image:'../images/Veh/sport/veh49.png',
+  name: 'Brabham BT62R',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: '29c16634-6102-4dd2-9fa7-287a93d58d21',
+  image:'../images/Veh/sport/veh39.png',
+  name: 'Mazda RX7',
+  price: 999,
+  rent: '--.--'
+}, {
+  id: 'ccd353b2-2156-4126-811d-974bda5f8c24',
+  image:'../images/Veh/other/veh59.png',
+  name: 'Polestar 1',
+  price: 499,
+  rent: '--.--'
+}, {
+  id: '1149a429-1f33-42b8-926b-9c2745444767',
+  image:'../images/Veh/other/veh55.png',
+  name: 'Cadillac Escalade',
+  price: 499,
+  rent: '--.--'
+}, {
+  id: '04d9d162-db0c-4182-ad65-272bfa69064a',
+  image:'../images/Veh/other/veh57.png',
+  name: 'Range Rover',
+  price: 499,
+  rent: '--.--'
+}];
+
+let productsHTML = ''
+products.forEach((product) => {
+  productsHTML += `
+        <div class="store-item">
+        <div class="store-item-up-cars">
+          <a href="../store/item.html"><img src="${product.image}" alt="expired or deleted"></a>
+        </div>
+          <div class="store-item-down">
+          <div class="store-item-info">
+          <div class="store-item-name">${product.name}</div>
+          <div class="store-price-buy">$${product.price / 100} &nbsp;<span style="color: white;"> :شراء </span> </div>
+          <div class="store-price-buy">$${product.rent} &nbsp;<span style="color: white;"> :إيجار </div>
+        </div>
+        <button class="btn-available js-add-to-cart" data-product-id="${product.id}">إضافة الى السلة</button>
+        </div>
+      </div>
+  `
+})
+
+document.querySelector('.js-car-products').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+
+    let validItem
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        validItem = item
+      }
+    })
+    if (validItem) {
+      validItem.quantity += 1
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      })
+    }
+    console.log(cart)
+  })
+})
+
+/* const restaurantProducts = [{
+  id: '',
+  image:'../images/house/castle.png',
+  name: '#1 Castle | قصر',
+  price: 2499,
+  rent: 999,
+}, {
+  id: '',
+  image:'../images/house/house.jpg',
+  name: '#1 Villa | فيلا',
+  price: 1999,
+  rent: 999,
+}, {
+  id: '',
+  image:'../images/house/house.jpg',
+  name: '#2 Villa | فيلا',
+  price: 1999,
+  rent: 999,
+}, {
+  id: '',
+  image:'../images/house/house.jpg',
+  name: '#3 Villa | فيلا',
+  price: 1899,
+  rent: 899,
+}, {
+  id: '',
+  image:'../images/house/house.jpg',
+  name: '#4 Villa | فيلا',
+  price: 1799,
+  rent: 799,
+}, {
+  id: '',
+  image:'../images/house/house.jpg',
+  name: '#5 Villa | فيلا',
+  price: 1699,
+  rent: 699,
+}, {
+  id: '',
+  image:'../images/house/house.jpg',
+  name: '#6 Villa | فيلا',
+  price: 1599,
+  rent: 599,
+}, {
+  id: '',
+  image:'../images/house/house.jpg',
+  name: '#7 Villa | فيلا',
+  price: 1499,
+  rent: 499,
+}, {
+  id: '',
+  image:'../images/house/house.jpg',
+  name: '#8 Villa | فيلا',
+  price: 1399,
+  rent: 399,
+}, {
+  id: '',
+  image:'../images/house/house.jpg',
+  name: '#9 Villa | فيلا',
+  price: 1399,
+  rent: 399,
+}, {
+  id: '',
+  image:'../images/service.jpg',
+  name: ' قراج مركبات | المستوى الثالث ' ,
+  price: 1999,
+  rent: 999,
+}, {
+  id: '',
+  image:'../images/service.jpg',
+  name: ' قراج مركبات | المستوى الثاني ' ,
+  price: 1499,
+  rent: 699,
+}, {
+  id: '',
+  image:'../images/service.jpg',
+  name: ' قراج مركبات | المستوى الأول ' ,
+  price: 999,
+  rent: 499,
+}] */
+
+/* let houseProductsHT = ''
+restaurantProducts.forEach((product) => {
+  houseProductsHT += `
+          <div class="store-item">
+        <div class="store-item-up-restaurants">
+          <a href="store/item.html"><img src="${product.image}" alt="expired or deleted"></a>
+        </div>
+        <div class="store-item-down">
+          <div class="store-item-info">
+            <div class="store-item-name">${product.name}</div>
+            <div class="store-price-buy"> $${product.price / 100} <span> : شراء</span> </div>
+            <div class="store-price-rent"> $${product.rent / 100} <span> : إيجار</span> </div>
+          </div>
+          <button class="btn-available">إضافة الى السلة</button>
+        </div>
+      </div>
+  `  
+}) 
+
+document.querySelector('.js-estate-products').innerHTML = houseProductsHT */
